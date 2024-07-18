891df55371e306461756bd65e5c01037ed617861
Hiba Fneish <153556261+hibafn97@users.noreply.github.com>
2024-07-18 17:56:08 +0300

    writing the printDrivers funtion

--- index.py
+++ index.py
@@ -1,3 +1,14 @@
+class Drivers:
+  def __init__(self):
+    self.drivers = {}
+
+  
+
+  def printDrivers(self):
+    for k,v in self.drivers.items():
+      print(f"{k} , {v[0]} , {v[1]}")
+  
+
 class Cities:# using adjacency matrix
 
   def __init__(self, V):
@@ -15,14 +26,15 @@ class Cities:# using adjacency matrix
     print()
 
   #O(1)
-  def addEdge(self, i, j, weight):
-    self.graph[i][j] = weight
-    self.graph[j][i] = weight
+  def addEdge(self, i, j):
+    self.graph[i][j] = 1
+    self.graph[j][i] = 1
 
   #O(1)
   def deleteEdge(self, i, j):
     self.graph[i][j] = 0
     self.graph[j][i] = 0
+
 
 def startMenu():
   i = int(input("1. To go to the drivers menu\n2. To go to the cities menu\n3. To exit the system\n"))
@@ -35,15 +47,22 @@ def startMenu():
   else:
     startMenu()
 
+def driversMenu():
+  i = int(input("1. To view all the drivers\n2. To add a driver\n3. To go back to main menu\n"))
 
-# def citiesMenu():
-#   i = int(input("1. Show cities\n2. Print neighboring cities\n3. Print Drivers delivering to city\n"))
+
+def citiesMenu():
+  i = int(input("1. Show cities\n2. Print neighboring cities\n3. Print Drivers delivering to city\n"))
   
-
-# def driversMenu():
-#   i = int(input("1. To view all the drivers\n2. To add a driver\n3. To go back to main menu\n"))
   
 drivers = {"ID0010":["Ali Tarhini","Beirut"],"ID002":["Salim Mhanna","Saida"],"ID003":["Sara jaber","Batroun"]}
 C = Cities(500)
 
+#startMenu()
   
+# D=Drivers()
+# D.addDriver("hiba","beirut")
+# D.addDriver("mohammad","akkar")
+# D.addDriver("Ali","tyre")
+# D.addDriver("sami","beirut")
+# D.printDrivers()
