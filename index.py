d8737312f00de7a53bcd465d56660637f7b3ea82
Hiba Fneish <153556261+hibafn97@users.noreply.github.com>
2024-07-18 18:33:45 +0300

    updating the linked list class

--- index.py
+++ index.py
@@ -10,30 +10,82 @@ class LinkedList:
   def __init__(self):
     self.head = None
     self.tail = None
-    self.size = 0  #how many element I have in my LL
+    self.size = 0  #how many nodes are in my LL
 
-  def addHead(self, info):  #O(1)
+  def addToHead(self, info):  #O(1)
     n = Node(info, None)
-
-    if self.size == 0:  # LL is empty
+    if self.size == 0:  # LL is empty, I have no nodes inside
       self.head = n
       self.tail = n
-      self.size += 1
+      self.size = 1
     else:
       n.next = self.head
       self.head = n
       self.size += 1
 
-  def addTail(self, info):  #O(1)
-    n = Node(info, None)
-    if self.size == 0:  # LL is empty
-      self.head = n
-      self.tail = n
-      self.size += 1
+  def addToTail(self, info):  #O(1)
+    if self.size == 0:
+      self.addToHead(info)
     else:
+      n = Node(info, None)
       self.tail.next = n
       self.tail = n
       self.size += 1
+
+
+  def deleteHead(self):  # O(1)
+    if self.size == 0:  # empty
+      return None
+    elif self.size == 1:
+      val = self.head.info
+      self.head = None
+      self.tail = None
+      self.size = 0
+      return val
+    else:
+      val = self.head.info
+      self.head = self.head.next
+      self.size -= 1
+      return val
+
+
+  def printLL(self):  #O(n), where n is the number of nodes in the list
+    i = self.head
+    while i != None:
+      print(i.info, "->", end="")
+      i = i.next
+    print()
+
+
+  def deleteTail(self): #O(n), where n is the length of my LL
+    if self.size<=1:
+      return self.deleteHead()
+    else:
+      val=self.tail.info
+      #loop to find the element before the last
+      i=self.head
+      while i.next.next!=None: #I did not reach the node before the last
+        i=i.next
+      #update the tail and its next
+      self.tail=i
+      self.tail.next=None
+      self.size-=1
+      return val
+
+  # remove the node that contains info
+  def removeNode(self,info):
+    pass
+    
+  # search for info in LL 
+  def search(self,info):
+    if self.size==0:
+      return False
+    current=self.head
+    while current!=None:
+      if current.info==info:
+        return True
+      current=current.next
+    return False
 
 class Drivers:
   def __init__(self):
@@ -65,7 +117,7 @@ class Cities:# using adjacency list
     if self.graph[i].search(j):
       return None
     else:
-      self.graph[i].addHead(j)
+      self.graph[i].addToHead(j)
   #O(V)
   def deleteEdge(self,i,j):
     self.graph[i].removeNode(j)
